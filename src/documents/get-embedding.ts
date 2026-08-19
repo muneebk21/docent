f87cdc2fbccc4289d5d5e1b2106b5368be08d9f9
MuneebKhan1996 <rmkhan1996@hotmail.com>
2026-08-19 23:12:51 +0500

    Static import in get-embedding changed to dynamic import

--- src/documents/get-embedding.ts
+++ src/documents/get-embedding.ts
@@ -1,6 +1,5 @@
-import { pipeline } from '@xenova/transformers';
-
 export async function getEmbedding(text: string): Promise<number[]> {
+  const { pipeline } = await import('@xenova/transformers');
   const extractor = await pipeline(
     'feature-extraction',
     'Xenova/all-MiniLM-L6-v2',
